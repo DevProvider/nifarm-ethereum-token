c9c8a4f21161ea535753e001c2b0fb426b1c4402
Khuram <khuram@devprovider.com>
2019-02-20 16:44:22 +0500

    My latest code

--- truffle.js
+++ truffle.js
@@ -12,20 +12,34 @@
  *   },
  */
 
+ var HDWalletProvider = require("truffle-hdwallet-provider");
+ var mnemonic = "soft moral grass same outside hard anger secret silly tell choice jaguar found state control";
+
 module.exports = {
+  
+    networks: {
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/2d24b86e8d884f5aa7a11e78dbb3c87d")
+      },
+      network_id: 3
+    }   
+  }
+
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  networks: {
-    development: {
-      host: '127.0.0.1',
-      port: '7545',
-      network_id: "*"
-    },
+  // networks: {
+  //   development: {
+  //     host: '127.0.0.1',
+  //     port: '7545',
+  //     network_id: "*"
+  //   },
   // rinkeby:{
   //   host: "localhost",s
   //   port: 8545,
   //   network_id: 4,
   //   gas: 4700000
   // }
-  }
+  //}
+
 };
